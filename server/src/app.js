f186691e82f583c38d5f7c280f7f39d00f563b6b
Vasken Khadar <vaskhder.cs@gmail.com>
2020-10-27 16:05:02 +1100

    - init node express app  - added dependecies including dotenv to manage env variables

---
+++ server/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, `./configs/.env.${process.env.NODE_ENV}`) });
+
+/* DB connection */
+
+mongoose.connect(`${process.env.MONGO_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on("connected", () => console.log("connected to mongodb"));
+db.on("disconnected", () => console.log("connection to disconnected"));
+db.on("error", (err) => console.log("connection error", err));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(helmet());
+
+/* API Routes */
+
+
+/* Errors handling */
+
+module.exports = app;
